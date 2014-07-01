200d15ca453ed2d3cf2c8d3461440c06b1ecfc3a
Syth Ryan <syth.ryan@banno.com>
2014-07-01 10:53:00 -0500

    role factory example

---
+++ chap3Mongo/src/main/scala/Role.scala
@@ -0,0 +1,21 @@
+abstract class Role { def canAccess(page: String): Boolean }
+
+class Root extends Role {
+	override def canAccess(page: String) = true
+}
+
+class SuperAnalyst extends Role {
+	override def canAccess(page: String) = page != "Admin"
+}
+
+class Analyst extends Role {
+	override def canAccess(page: String) = false
+}
+
+object Role {
+	def apply(roleName: String) = roleName match {
+		case "root" => new Root
+		case "superAnalyst" => new SuperAnalyst
+		case "analyst" => new Analyst
+	}
+}
