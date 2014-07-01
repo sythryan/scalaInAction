2e3c1f00b712d0c3d89ac2980685689f8727f9d4
Syth Ryan <syth.ryan@banno.com>
2014-07-01 14:04:46 -0500

    added for comp to private class DB

--- chap3Mongo/src/main/scala/DB.scala
+++ chap3Mongo/src/main/scala/DB.scala
@@ -1,8 +1,11 @@
 package com.scalainaction.mongo
 
 import com.mongodb.casbah.MongoDB
+import scala.collection.convert.Wrappers._
 
 class DB private(val underlying: MongoDB) {
+	def collectionNames = for(name <- new
+			JSetWrapper(underlying.getCollectionNames)) yield name
 }
 
 object DB {
--- chap3Mongo/src/main/scala/classPractice.scala
+++ chap3Mongo/src/main/scala/classPractice.scala
@@ -1,17 +1,18 @@
 package com.scalainaction.mongo
 
 import com.mongodb._
+import com.mongodb.casbah.Imports._
 
 class MyMongoClient(val host:String, val port:Int) {
   require(host != null, "Yout have to provide a host name")
-	private val underlying = new Mongo(host, port)
+	private val underlying = new MongoConnection(new Mongo(host, port))
 	def this() = this("127.0.0.1", 27017)
 
   def version = underlying.getVersion
 
   def dropDB(name:String) = underlying.dropDatabase(name)
 
-  def createDB(name:String) = underlying.getDB(name)
+  def createDB(name:String) = DB(underlying.getDB(name))
 
-  def db(name:String) = underlying.getDB(name)
+  def db(name:String) = DB(underlying.getDB(name))
 }
